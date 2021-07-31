78ee9b7e689b7df36b8b72521a236bebc1f9adc6
glpla <1942194789@qq.com>
2021-07-31 16:06:10 +0800

    up

--- data2021/intern.js
+++ data2021/intern.js
@@ -3,8 +3,7 @@ const intern = {
     college: '桂林学院理工学院',
     grade: '2018级',
     major: '软件工程',
-    event: '实习情况',
-    date: '2021-07-27'
+    event: '实习情况'
   },
   admin: {
     name: '张树彬',
@@ -14,27 +13,51 @@ const intern = {
     email: '1942194789@qq.com'
   },
   students: [{
+    _id: 1001,
+    avatar: 'imgs/intern/y1.jpg',
     name: '杨广昭',
     cell: '18269269895',
-    internSta: '已实习',
-    gradSta: '已开始',
-    avatar: 'imgs/intern/y1.jpg',
-    comments: '请按时签到',
-    unit: '东软睿道教育信息技术有限公司',
-    location: '广西桂林七星区湖塘总部经济园',
-    contact: '李宽行',
-    contactCell: '18027108921'
+    grad: '已开始',
+    intern: [{
+      date: '2021-07-27',
+      sta: '已实习',
+      post: 'imgs/intern/y1.jpg',
+      comments: '请按时签到00'
+    }, {
+      date: '2021-08-03',
+      sta: '已实习',
+      post: 'imgs/intern/y0.jpg',
+      comments: '请按时签到01'
+    }],
+    unit: {
+      title: '东软睿道教育信息技术有限公司',
+      location: '广西桂林七星区湖塘总部经济园',
+      contact: '李宽行',
+      cell: '18027108921'
+    }
   }, {
+    _id: 1002,
+    avatar: 'imgs/intern/l1.jpg',
     name: '卢俊铭',
     cell: '15777457452',
-    internSta: '已实习',
-    gradSta: '已开始',
-    avatar: 'imgs/intern/l1.jpg',
-    comments: '请按时签到',
-    unit: '东软睿道教育信息技术有限公司',
-    location: '广西桂林七星区湖塘总部经济园',
-    contact: '李宽行',
-    contactCell: '18027108921'
+    grad: '已开始',
+    intern: [{
+      date: '2021-07-27',
+      sta: '已实习',
+      post: 'imgs/intern/l1.jpg',
+      comments: '请按时签到10'
+    }, {
+      date: '2021-08-03',
+      sta: '已实习',
+      post: 'imgs/intern/l0.jpg',
+      comments: '请按时签到11'
+    }],
+    unit: {
+      title: '东软睿道教育信息技术有限公司',
+      location: '广西桂林七星区湖塘总部经济园',
+      contact: '李宽行',
+      cell: '18027108921'
+    }
   }]
 }
 export default intern;
