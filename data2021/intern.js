849dc74dba5b5838174861c61028221862f3daf8
glpla <1942194789@qq.com>
2021-07-31 13:13:07 +0800

    intern

---
+++ data2021/intern.js
@@ -0,0 +1,40 @@
+const intern = {
+  title: {
+    college: '桂林学院理工学院',
+    grade: '2018级',
+    major: '软件工程',
+    event: '实习情况',
+    date: '2021-07-27'
+  },
+  admin: {
+    name: '张树彬',
+    nickName: 'cnplaman',
+    cell: '13707735481',
+    unit: 'pla54414',
+    email: '1942194789@qq.com'
+  },
+  students: [{
+    name: '杨广昭',
+    cell: '18269269895',
+    internSta: '已实习',
+    gradSta: '已开始',
+    avatar: 'imgs/intern/y1.jpg',
+    comments: '请按时签到',
+    unit: '东软睿道教育信息技术有限公司',
+    location: '广西桂林七星区湖塘总部经济园',
+    contact: '李宽行',
+    contactCell: '18027108921'
+  }, {
+    name: '卢俊铭',
+    cell: '15777457452',
+    internSta: '已实习',
+    gradSta: '已开始',
+    avatar: 'imgs/intern/l1.jpg',
+    comments: '请按时签到',
+    unit: '东软睿道教育信息技术有限公司',
+    location: '广西桂林七星区湖塘总部经济园',
+    contact: '李宽行',
+    contactCell: '18027108921'
+  }]
+}
+export default intern;
