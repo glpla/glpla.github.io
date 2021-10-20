c52b7b947528e5790584ca3bc3174694336b663d
glpla <1942194789@qq.com>
2021-10-20 13:42:32 +0800

    e

---
+++ data2021/e.js
@@ -0,0 +1,24 @@
+const quiz={
+  match:[{
+    id:7001,
+    keyword:'hardware',
+    desc:'123123123'
+  },{
+    id:7002,
+    keyword:'hardware',
+    desc:'11111111'
+  },{
+    id:7003,
+    keyword:'hardware',
+    desc:'2222222222'
+  },{
+    id:7004,
+    keyword:'hardware',
+    desc:'3333333333333'
+  },{
+    id:7005,
+    keyword:'hardware',
+    desc:'444444444444'
+  }]
+}
+export default quiz;
