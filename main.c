26c29bd124042b5164ffc5889b5ed86897326c50
glpla <1942194789@qq.com>
2022-03-14 19:57:25 +0800

    insert

--- main.c
+++ main.c
@@ -3,77 +3,128 @@
 #include <string.h>
 typedef struct Node
 {
-  int data;
-  struct Node *next;
+    int data;
+    struct Node *next;
 } Node;
 
 typedef struct
 {
-  Node *head;
-  int len;
+    Node *head;
+    int len;
 } List;
 
 void init(List *list)
 {
-  list->head = (Node *)malloc(sizeof(Node));
-  list->head->next = NULL;
-  list->len = 0;
+    list->head = (Node *)malloc(sizeof(Node));
+    list->head->next = NULL;
+    list->len = 0;
 }
 
 int isEmpty(List *list)
 {
-  return list->len == 0;
+    return list->len == 0;
 }
 
 int len(List *list)
 {
-  return list->len;
+    return list->len;
 }
 void insertHead(List *list, int el)
 {
-  Node *node = (Node *)malloc(sizeof(Node));
-  node->data = el;
-  node->next = list->head->next;
-  list->head->next = node;
-  list->len++;
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = el;
+    node->next = list->head->next;
+    list->head->next = node;
+    list->len++;
 }
 void insertTail(List *list, int el)
 {
-  Node *node = (Node *)malloc(sizeof(Node)), *p, *cur;
-  node->data = el;
-  node->next = NULL;
-  cur = list->head;
-  p = cur->next;
-  while (p)
-  {
-    printf("data=%d\n", p->data);
-    cur = cur->next;
-    p = p->next;
-  }
-  cur->next = node;
-  list->len++;
+    Node *node = (Node *)malloc(sizeof(Node)), *p, *cur;
+    node->data = el;
+    node->next = NULL;
+    cur = list->head;
+    p = cur->next;
+    while (p)
+    {
+        printf("data=%d\n", p->data);
+        cur = cur->next;
+        p = p->next;
+    }
+    cur->next = node;
+    list->len++;
+}
+
+int insertAt(List *list, int loc, int el)
+{
+    if (loc < 0 || loc > list->len)
+    {
+        return -1;
+    }
+    if (!loc)
+    {
+        insertHead(list, el);
+        return 0;
+    }
+    if (loc == list->len)
+    {
+        insertTail(list, el);
+        return 0;
+    }
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = el;
+    node->next = NULL;
+    Node *p = list->head->next;
+    int ind = 0;
+    while (ind < loc - 1)
+    {
+        p = p->next;
+        ind++;
+    }
+    node->next = p->next;
+    p->next = node;
+    return 0;
+}
+
+void delMax(List *list)
+{
+    Node *cur, *p, *max;
+    cur = list->head;
+    p = list->head->next;
+    max = p;
+    while (p)
+    {
+        if (p->data > cur->data)
+        {
+            max = p;
+            cur = cur->next;
+        }
+        p = p->next;
+    }
 }
 
 void display(List *list)
 {
-  Node *p = list->head->next;
-  while (p)
-  {
-    printf("%d\n", p->data);
-    p = p->next;
-  }
+    Node *p = list->head->next;
+    while (p)
+    {
+        printf("%d\n", p->data);
+        p = p->next;
+    }
 }
 int main(void)
 {
-  List *list;
-  init(list);
-  insertHead(list, 1);
-  insertHead(list, 2);
-  insertHead(list, 3);
-  display(list);
-  insertTail(list, 1);
-  insertTail(list, 2);
-  insertTail(list, 3);
-  display(list);
-  return 0;
+    List *list;
+    init(list);
+    insertHead(list, 1);
+    insertHead(list, 2);
+    insertHead(list, 3);
+    display(list);
+    insertTail(list, 1);
+    insertTail(list, 2);
+    insertTail(list, 3);
+    insertAt(list, 0, 11);
+    insertAt(list, 7, 77);
+    insertAt(list, 4, 77);
+    display(list);
+    return 0;
 }
