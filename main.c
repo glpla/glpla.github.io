c9494330b8fa11a68f6f064307f420bf55ee5815
glpla <1942194789@qq.com>
2022-03-15 19:29:54 +0800

    list

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 typedef struct Node
 {
     int data;
@@ -17,6 +17,7 @@ void init(List *list)
 {
     list->head = (Node *)malloc(sizeof(Node));
     list->head->next = NULL;
+    list->head->data = -1;
     list->len = 0;
 }
 
@@ -27,6 +28,7 @@ int isEmpty(List *list)
 
 int len(List *list)
 {
+    printf("length=%d\n", list->len);
     return list->len;
 }
 void insertHead(List *list, int el)
@@ -46,18 +48,107 @@ void insertTail(List *list, int el)
     p = cur->next;
     while (p)
     {
-        printf("data=%d\n", p->data);
         cur = cur->next;
         p = p->next;
     }
     cur->next = node;
     list->len++;
 }
+int destroy(List *list)
+{
+    if (!list->len)
+    {
+        return -1;
+    }
+    Node *p = list->head->next;
+    while (p)
+    {
+        list->head->next = p->next;
+        free(p);
+        p = list->head->next;
+    }
+    list->len = 0;
+    return 0;
+}
+
+int getAt(List *List, int loc)
+{
+    printf("len=%d\n", List->len);
+    if (loc > -1 && loc < List->len)
+    {
+        Node *p = List->head->next;
+        int ind = 0;
+        while (ind < loc)
+        {
+            p = p->next;
+            ind++;
+        }
+        printf("res=%d\n", p->data);
+        return p->data;
+    }
+    printf("index overfolw\n");
+    return -1;
+}
+int indexOf(List *list, int data)
+{
+    Node *p = list->head->next;
+    int loc = 0;
+    while (p)
+    {
+        if (p->data == data)
+        {
+            printf("item %d found at %d \n", p->data, loc);
+            return loc;
+        }
+        p = p->next;
+        loc++;
+    }
+    printf("item %d not found\n", data);
+    return -1;
+}
+
+int delAt(List *list, int loc)
+{
+    if (loc > -1 && loc < list->len)
+    {
+        Node *p, *cur;
+        cur = list->head;
+        p = cur->next;
+        for (int i = 0; i < loc; i++)
+        {
+            cur = cur->next;
+            p = p->next;
+        }
+        cur->next = p->next;
+        free(p);
+        list->len--;
+        return 0;
+    }
+    return -1;
+}
+// indexOf() and delAt()
+int delBy(List *list, int el)
+{
+    Node *p = list->head->next;
+    int loc = 0;
+    while (p)
+    {
+        if (p->data == el)
+        {
+            delAt(list, loc);
+            return loc;
+        }
+        p = p->next;
+        loc++;
+    }
+    return -1;
+}
 
 int insertAt(List *list, int loc, int el)
 {
-    if (loc < 0 || loc > list->len)
+    if (loc < 0 || loc > list->len - 1)
     {
+        printf("insert index overfollow\n");
         return -1;
     }
     if (!loc)
@@ -82,34 +173,75 @@ int insertAt(List *list, int loc, int el)
     }
     node->next = p->next;
     p->next = node;
+    list->len++;
     return 0;
 }
 
-void delMax(List *list)
+// assumed head->data=-1
+int delMax(List *list)
 {
-    Node *cur, *p, *max;
-    cur = list->head;
-    p = list->head->next;
+    if (!list->len)
+    {
+        printf("list is empty\n");
+        return -1;
+    }
+    if (list->len == 1)
+    {
+        printf("maxItem %d deleted\n", list->head->next->data);
+        free(list->head->next);
+        list->head->next = NULL;
+        list->len = 0;
+        return 0;
+    }
+    Node *pre, *p, *max, *maxPre;
+    pre = list->head->next;
+    maxPre = pre;
+    p = pre->next;
     max = p;
     while (p)
     {
-        if (p->data > cur->data)
+        if (p->data > max->data)
         {
             max = p;
-            cur = cur->next;
+            maxPre = pre;
         }
+        pre = pre->next;
         p = p->next;
     }
+    maxPre->next = max->next;
+    printf("maxItem %d deleted\n", max->data);
+    free(max);
+    list->len--;
+    return 0;
+}
+
+int updateAt(List *list, int loc, int data)
+{
+    if (loc > -1 && loc < list->len)
+    {
+        Node *p = list->head->next;
+        for (int i = 0; i < loc; i++)
+        {
+            p = p->next;
+        }
+        p->data = data;
+        printf("item at %d updated\n", loc);
+        return 0;
+    }
+    printf("update index overfollow\n");
+    return -1;
 }
 
 void display(List *list)
 {
+    printf("start\n");
     Node *p = list->head->next;
     while (p)
     {
         printf("%d\n", p->data);
         p = p->next;
     }
+    printf("end\n");
 }
 int main(void)
 {
@@ -118,13 +250,15 @@ int main(void)
     insertHead(list, 1);
     insertHead(list, 2);
     insertHead(list, 3);
-    display(list);
     insertTail(list, 1);
     insertTail(list, 2);
     insertTail(list, 3);
     insertAt(list, 0, 11);
-    insertAt(list, 7, 77);
     insertAt(list, 4, 77);
+    display(list);
+    delMax(list);
+    delMax(list);
+    updateAt(list, 1, 80);
     display(list);
     return 0;
 }
