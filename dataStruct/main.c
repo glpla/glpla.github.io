6dc032e45a95ec136497f9c6031b6b306c6b7c41
glpla <1942194789@qq.com>
2022-04-15 07:26:35 +0800

    bg color

--- dataStruct/main.c
+++ dataStruct/main.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define uchar unsigned char
-#define uint unsigned int
-
 int main(void)
 {
-    uchar ch = 0xEE;
-    printf("%#x\n", ch);
-    ch |= (1 << 4);
-    printf("%#x\n", ch);
-    // for (int i = 0; i < 8; i++)
-    // {
-    //     printf("%#x\n", ch);
-    //     ch = ~ch;
-    //     ch = ch << 1;
-    //     ch = ~ch;
-    // }
-
+    printf("hello,makefile.\n");
     return 0;
 }
