08865176194d63b18d1a900c355c0e4d00493ed2
glpla <1942194789@qq.com>
2022-05-08 07:36:59 +0800

    exp2

--- dataStruct/main.c
+++ dataStruct/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#if defined(VER1)
+#pragma message("ver 1.0")
+#define VER "ver 1.0"
+#elif defined(VER2)
+#pragma message("ver 2.0")
+#define VER "ver 2.0"
+#else
+#pragma message("ver not provided")
+#define VER "null"
+#endif
 int main(void)
 {
-    printf("hello,makefile.\n");
+    printf("%s\n", VER);
     return 0;
 }
