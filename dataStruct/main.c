4c852835fd22624ef0f4fec44ecd467b82b133b9
glpla <1942194789@qq.com>
2022-05-24 17:19:27 +0800

    zigbee

--- dataStruct/main.c
+++ dataStruct/main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-#if defined(VER1)
-#pragma message("ver 1.0")
-#define VER "ver 1.0"
-#elif defined(VER2)
-#pragma message("ver 2.0")
-#define VER "ver 2.0"
-#else
-#pragma message("ver not provided")
-#define VER "null"
-#endif
+
 int main(void)
 {
-    printf("%s\n", VER);
+    char *str = "1234";
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%c\n", *str++);
+    }
+
     return 0;
 }
