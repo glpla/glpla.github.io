d95db16c81dd2de20bf1d413e41deb1ebbb81db0
glpla <1942194789@qq.com>
2022-05-29 17:59:36 +0800

    graph

--- dataStruct/graph.c
+++ dataStruct/graph.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct vNode
-{
-    int data;
-    int w;
-    ENode *first;
-} VNode;
-
 typedef struct eNode
 {
     int adjV;
     int w;
     struct eNode *nextAdjV;
 } ENode;
-
 typedef struct
 {
     int vSize;
     int eSize;
     ENode **arr;
 } Graph;
-
 void init(Graph *g, int size)
 {
     int i;
@@ -33,10 +24,9 @@ void init(Graph *g, int size)
         {
             g->arr[i] = NULL;
         }
+        printf("init done.\n");
     }
 }
-// free ENode
-// free Graph
 void destroy(Graph *g)
 {
     int i;
@@ -54,46 +44,29 @@ void destroy(Graph *g)
     }
     free(g->arr);
 }
-// find edge in vertex n
-// where you find the vertex, you find the edge
 int edgeFind(Graph *g, int ind, int vertex)
 {
-    // the validation of n and vertex
-    // by yourself
-
     ENode *p = g->arr[ind];
     while (p && p->adjV != vertex)
-    {
         p = p->nextAdjV;
-    }
     if (!p)
-    {
         return -1;
-    }
     else
-    {
         return 0;
-    };
 }
 int edgeInsert(Graph *g, int ind, int vertex, int w)
 {
-    // the validation of n and vertex
-    // by yourself
-
     if (!edgeFind(g, ind, vertex))
-    {
         return -1;
-    }
-
     ENode *p = (ENode *)malloc(sizeof(ENode));
     p->adjV = vertex;
     p->w = w;
     p->nextAdjV = g->arr[ind];
     g->arr[ind] = p;
     g->eSize++;
+    printf("Insert done.\n");
     return 0;
 }
-
 int edgeDel(Graph *g, int ind, int vertex)
 {
     // the validation of n and vertex
@@ -101,7 +74,7 @@ int edgeDel(Graph *g, int ind, int vertex)
     ENode *p, *q;
     p = g->arr[ind];
     q = NULL;
-    while (p && p->nextAdjV != vertex)
+    while (p && p->adjV != vertex)
     {
         q = p;
         p = p->nextAdjV;
@@ -109,18 +82,52 @@ int edgeDel(Graph *g, int ind, int vertex)
     if (!p)
         return -1;
     if (q)
+    {
         q->nextAdjV = p->nextAdjV;
+    }
     else
+    {
         g->arr[ind] = p->nextAdjV;
+    }
     free(p);
     g->eSize--;
     return 0;
 }
-
+void DFS(int vertex, int visted[], Graph *g)
+{
+    ENode *w;
+    printf("%d ", vertex);
+    visted[vertex] = 1;
+    for (w = g->arr[vertex]; w; w = w->nextAdjV)
+    {
+        if (!visted[w->adjV])
+        {
+            DFS(w->adjV, visted, g);
+        }
+    }
+}
+void DFSGraph(Graph *g)
+{
+    int i;
+    int *visted = (int *)malloc(g->vSize * sizeof(int));
+    for (i = 0; i < g->vSize; i++)
+    {
+        visted[i] = 0;
+        // printf("i=%d\n", i);
+    }
+    for (i = 0; i < g->vSize; i++)
+    {
+        if (!visted[i])
+        {
+            DFS(i, visted, g);
+        }
+    }
+    free(visted);
+}
 int main(void)
 {
-    int arr[5];
-    printf("arr=%d\n", sizeof(arr));
-
+    Graph *g;
+    int i;
+    init(g, 5);
     return 0;
 }
