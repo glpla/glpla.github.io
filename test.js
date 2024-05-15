5faf484d419d7a50209486fcaba26f74209e544a
glpla <1942194789@qq.com>
2024-05-15 21:59:24 +0800

    web

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
-app.listen(3000, () => {
-  console.log('hi');
-})
+const config = {
+  "author": "glpla",
+  "desc": "only for test",
+  "url": "glpla.github.io",
+  "version": "1.0.0",
+}
+
+const greeting = (name) => {
+  console.log(`${name}, hi, there.`)
+}
+export { config, greeting }
