a3b55a788dcc3d8a9dedd8890356c49d1158ffdb
glpla <1942194789@qq.com>
2024-05-29 18:14:44 +0800

    index

--- web/lib/companies.js
+++ web/lib/companies.js
@@ -1,5 +1,17 @@
 const head = document.querySelector("header");
 const back = document.querySelector(".back");
+const burger = document.querySelector('.burger')
+
+document.addEventListener('scrollStart', () => {
+  if (head.classList.contains('open')) {
+    head.classList.remove('open')
+  }
+})
+
+burger.addEventListener('click', () => {
+  head.classList.toggle('open')
+})
+
 window.addEventListener("scroll", () => {
   let h = head.getBoundingClientRect().height;
   if (window.pageYOffset - h > 200) {
@@ -15,35 +27,55 @@ window.addEventListener("scroll", () => {
     back.style.display = "none";
   }
 });
-const glide = new Glide(".glide");
-const captionEl = document.querySelectorAll(".glide-caption");
-glide.on(["mount.after", "run.after"], () => {
-  const caption = captionEl[glide.index];
+
+// scrollReveal option
+const revealOption = {
+  delay: 300,
+  distance: "50px",
+  duration: 500,
+  easing: "ease-in-out",
+  origin: "bottom",
+};
+
+// swiper
+const swiperInfo = document.querySelectorAll('.swiper-info')
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.on('slideChange', () => {
+  console.log('改变了，realIndex为' + swiper.realIndex);
   anime({
-    targets: caption.children,
+    targets: swiperInfo[swiper.realIndex].children,
     opacity: [0, 1],
     duration: 400,
     easing: "linear",
     delay: anime.stagger(400, { start: 300 }),
     translateY: [anime.stagger(40, 10), 0],
-  });
-});
-glide.on("run.before", () => {
-  document.querySelectorAll(".glide-caption>*").forEach((el) => {
-    el.style.opacity = 0;
-  });
-});
-glide.mount();
+  })
+}
+)
 
 // isotope
-
 const isotope = new Isotope(".item-wrap", {
-  layoutMode: "fitRows",
   itemSelector: ".item",
+  layoutMode: 'fitRows',
+  percentPosition: true,
+  fitRows: {
+    gutter: '.gutter-sizer'
+  },
 });
 const btns = document.querySelector(".btns-wrap");
 btns.addEventListener("click", (e) => {
-  // keep the name same
   let { target } = e;
   let ind = target.getAttribute("data-filter");
   console.log(ind);
@@ -57,43 +89,34 @@ btns.addEventListener("click", (e) => {
 });
 
 // scrollReveal
-const revealOption = {
-  delay: 300,
-  distance: "50px",
-  duration: 500,
-  easing: "ease-in-out",
-  origin: "bottom",
-};
-ScrollReveal().reveal(".feature", { ...revealOption, interval: 350 });
+ScrollReveal().reveal(".feature-item", { ...revealOption, interval: 350 });
 ScrollReveal().reveal(".service-item", { ...revealOption, interval: 350 });
-ScrollReveal().reveal(".data-wrap", {
-  beforeReveal: (el) => {
-    anime: ({
-      targets: '.data-item .num',
-      value: el => {
-        console.log(el.innerHTML);
-        return [0, el.innerHTML]
-      },
-      // innerHTML: el => {
-      //   return [0, el.innerHTML]
-      // },
+ScrollReveal().reveal(".team-item", { ...revealOption, interval: 350 });
+ScrollReveal().reveal(".news-item", { ...revealOption, interval: 350 });
+ScrollReveal().reveal(".data-item", {
+  afterReveal: (el) => {
+    let ipt = el.querySelector('.num');
+    console.log('ipt', ipt);
+    anime({
+      targets: ipt,
+      value: [0, ipt.value],
       duration: 2000,
       round: 1,
-      easing: 'easeInExpo'
+      easing: 'easeInExpo',
     })
   }
 });
+const animeIpt = () => {
+  anime: ({
+    targets: '.data-item .num',
+    value: [0, 234],
+    duration: 2000,
+    round: 1,
+    easing: 'easeInExpo'
+  })
+}
 
-const scroll = new SmoothScroll('nav a[href*="#"], .back a[href*="#"]', {
-  header: 'header',
-  offset: 80
-})
-document.addEventListener('scrollStart', () => {
-  if (head.classList.contains('open')) {
-    head.classList.remove('open')
-  }
-})
-const burger = document.querySelector('.burger')
-burger.addEventListener('click', () => {
-  head.classList.toggle('open')
-})
+// const scroll = new SmoothScroll('nav a[href*="#"], .back a[href*="#"]', {
+//   header: 'header',
+//   offset: 80
+// })
