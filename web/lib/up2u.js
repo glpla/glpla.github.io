d78f6a58f4fee854b97035693f80b042dfbb8414
glpla <1942194789@qq.com>
2024-06-12 16:45:55 +0800

    web

--- web/lib/up2u.js
+++ web/lib/up2u.js
@@ -23,27 +23,30 @@
 //   }
 //   return json;
 // }
+
 // msg
 let msgBox = document.querySelector('.msg-box')
-let msgs = fetch('../../utils/data/data2023/msg.json')
+let msgs = fetch('/utils/data/data2023/msg.json')
   .then(res => res.json())
   .then(res => {
-    console.log(res);
-    msgBox.innerHTML = res.map(item =>
+    msgBox.innerHTML = res.map((item, ind) =>
       `
       <div class="msg-item">
-        <div class="msg-title">${item.name}</div>
+        <div class="msg-title"><img src="https://glpla.github.io/utils/avatar/avatar${ind}.png" alt=""> ${item.name}</div>
         <div class="msg-mail">${item.mail}</div>
         <div class="msg-cont">${item.msg}</div>
       </div>
       `
     ).join('')
   })
+
 // countdown
 let time = document.querySelector('#time')
-let inter = setInterval(ticking, 1000)
 let y, m, d, hh, mm, ss
-function ticking() {
+const digit2 = (num) => {
+  return num > 9 ? num : '0' + num
+}
+const ticking = () => {
   date = new Date();
   y = date.getFullYear();
   m = date.getMonth() + 1;
@@ -54,6 +57,17 @@ function ticking() {
   time.innerHTML = `${y}年${digit2(m)}月${digit2(d)}日 ${digit2(hh)}:${digit2(mm)}:${digit2(ss)}`
 }
 ticking()
-function digit2(num) {
-  return num > 9 ? num : '0' + num
-}
+
+let inter = setInterval(ticking, 1000)
+
+const revealOption = {
+  delay: 300,
+  distance: "50px",
+  duration: 500,
+  easing: "ease-in-out",
+  interval: 350
+};
+// ScrollReveal().reveal("h3", { ...revealOption, origin: "bottom" });
+// ScrollReveal().reveal("figcaption", { ...revealOption, origin: "top" });
+// ScrollReveal().reveal(".msg-item", { ...revealOption, origin: "top" });
+// ScrollReveal().reveal(".help .help-item", { ...revealOption, origin: "top" });
