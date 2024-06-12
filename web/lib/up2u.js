fee85318106b7f150db51607c753b642c696ba7b
glpla <1942194789@qq.com>
2024-06-12 17:24:00 +0800

    handleMediaChange

--- web/lib/up2u.js
+++ web/lib/up2u.js
@@ -24,6 +24,11 @@
 //   return json;
 // }
 
+// main-nav
+window.addEventListener('scroll', (e) => {
+  mainNav.classList.toggle('active', window.scrollY > 150)
+})
+
 // msg
 let msgBox = document.querySelector('.msg-box')
 let msgs = fetch('/utils/data/data2023/msg.json')
@@ -60,13 +65,74 @@ ticking()
 
 let inter = setInterval(ticking, 1000)
 
-const revealOption = {
-  delay: 300,
-  distance: "50px",
-  duration: 500,
-  easing: "ease-in-out",
-  interval: 350
-};
+// swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  autoplay: true,
+  grabCursor: true,
+  speed: 1000,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  // centeredSlides: true,
+  mousewheel: true,
+  // effect: 'coverflow',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+  // on: {
+  //   click: (e) => {
+  //     console.log('hi', e.activeIndex, e.realIndex, e.autoplay);
+  //     e.autoplay = false
+  //   },
+  // },
+});
+
+// 定义媒体查询
+var mediaQuery = window.matchMedia("(max-width: 768px)");
+
+// 监听媒体查询的变化
+mediaQuery.addListener(handleMediaChange);
+
+// 初始化处理函数
+handleMediaChange(mediaQuery);
+
+function handleMediaChange(mediaQuery) {
+  if (mediaQuery.matches) {
+    // 当屏幕宽度小于或等于768px时执行的代码
+    console.log("屏幕宽度小于或等于768px，执行相应的JS代码...");
+    // 这里可以调用特定的函数或者改变DOM等
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+    })
+    clearInterval(inter);
+  } else {
+    // 当屏幕宽度大于768px时执行的代码
+    console.log("屏幕宽度大于768px，执行其他的JS代码...");
+    // 执行其他逻辑
+  }
+}
+// const revealOption = {
+//   delay: 300,
+//   distance: "50px",
+//   duration: 500,
+//   easing: "ease-in-out",
+//   interval: 350
+// };
 // ScrollReveal().reveal("h3", { ...revealOption, origin: "bottom" });
 // ScrollReveal().reveal("figcaption", { ...revealOption, origin: "top" });
 // ScrollReveal().reveal(".msg-item", { ...revealOption, origin: "top" });
