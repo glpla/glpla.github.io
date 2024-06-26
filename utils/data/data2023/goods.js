1af20cf0a07d2590527202c7db3a5ef45ae87a86
glpla <1942194789@qq.com>
2024-06-26 21:45:39 +0800

    web

--- utils/data/data2023/goods.js
+++ utils/data/data2023/goods.js
@@ -2,7 +2,7 @@ let goods = [
   {
     "id": 1001,
     "title": "耐热无汞玻璃水杯",
-    "picUrl": "../imgs/1.jpg",
+    "picUrl": "/utils/view/view0.jpg",
     "price": 3,
     "rate": 4,
     "desc": "8 cups a day",
@@ -11,7 +11,7 @@ let goods = [
   {
     "id": 1002,
     "title": "冲刺练习手册",
-    "picUrl": "../imgs/2.jpg",
+    "picUrl": "/utils/view/view2.jpg",
     "price": 10,
     "rate": 5,
     "desc": "more reed, more score",
@@ -20,7 +20,7 @@ let goods = [
   {
     "id": 1003,
     "title": "多色记号笔",
-    "picUrl": "../imgs/3.jpg",
+    "picUrl": "/utils/view/view3.jpg",
     "price": 5,
     "rate": 3,
     "desc": "make colorful mark",
@@ -29,7 +29,7 @@ let goods = [
   {
     "id": 1004,
     "title": "耐压防水多功能书包",
-    "picUrl": "../imgs/4.jpg",
+    "picUrl": "/utils/view/view4.jpg",
     "price": 20,
     "rate": 4,
     "desc": "little but big",
@@ -38,7 +38,7 @@ let goods = [
   {
     "id": 1005,
     "title": "2B铅笔",
-    "picUrl": "../imgs/5.jpg",
+    "picUrl": "/utils/view/view5.jpg",
     "price": 3,
     "rate": 2,
     "desc": "good for you",
@@ -47,7 +47,7 @@ let goods = [
   {
     "id": 1006,
     "title": "U盘",
-    "picUrl": "../imgs/6.jpg",
+    "picUrl": "/utils/view/view6.jpg",
     "price": 68,
     "rate": 5,
     "desc": "safe disk, safe data",
@@ -56,7 +56,7 @@ let goods = [
   {
     "id": 1007,
     "title": "涂改液",
-    "picUrl": "../imgs/7.jpg",
+    "picUrl": "/utils/view/view7.jpg",
     "price": 6,
     "rate": 3,
     "desc": "all errors can be erased",
@@ -65,7 +65,7 @@ let goods = [
   {
     "id": 1008,
     "title": "白桃橡皮",
-    "picUrl": "../imgs/8.jpg",
+    "picUrl": "/utils/view/view8.jpg",
     "price": 2,
     "rate": 4,
     "desc": "clear study, clear class",
@@ -74,7 +74,7 @@ let goods = [
   {
     "id": 1009,
     "title": "USB风扇",
-    "picUrl": "../imgs/9.jpg",
+    "picUrl": "/utils/view/view9.jpg",
     "price": 18,
     "rate": 5,
     "desc": "be a cool guy",
