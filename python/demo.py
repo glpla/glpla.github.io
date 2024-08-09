f2e8ae4232cf6fde99667ddfb32785ed7a7a83dc
glpla <1942194789@qq.com>
2024-08-10 00:13:13 +0800

    python

--- python/demo.py
+++ python/demo.py
@@ -1,5 +1,4 @@
-chars = list("hi,there.")
-nums = list(range(10))
-print(nums[-1])
-print(chars[-2])
-print(nums + chars)
+stus = ["张三", "李明四", "王五"]
+# stus.sort(key=lambda x: len(x["name"]))
+stus.sort(key=len)
+print(stus)
