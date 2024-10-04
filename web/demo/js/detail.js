556840e15139e5c24b6f92d40fb893ae95af15c6
glpla <1942194789@qq.com>
2024-10-04 22:30:08 +0800

    swiper

--- web/demo/js/detail.js
+++
@@ -1,8 +0,0 @@
-$('.colors span').click(function () {
-  $('.colors span').removeClass('active');
-  $(this).addClass('active');
-  $('.card-box').css('background', $(this).attr('data-color'));
-  $('.pic').css('background-image', $(this).attr('data-pic'));
-  $('.price').css('color', $(this).attr('data-color'));
-  $('.btn').css('color', $(this).attr('data-color'));
-})
