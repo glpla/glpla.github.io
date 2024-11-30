535f9fb3054e7dd21f25805f4087a2c945967967
glpla <1942194789@qq.com>
2024-11-30 22:24:18 +0800

    ham

--- utils/custom/ham2024.js
+++ utils/custom/ham2024.js
@@ -1,50 +1,26 @@
 let ind = 0;
+let header = document.querySelector('header');
 let navItems = document.querySelectorAll('.nav-item')
 let navConts = document.querySelectorAll('.nav-cont')
 let nav = document.querySelector('.nav');
 let ham = document.querySelector('.ham')
-
-const hamToggle = () => {
-  if (ind) {
-    ind = 0;
-    // ham.classList.remove('icon-close');
-    // ham.classList.add('icon-menu');
-    ham.className = 'iconfont icon-menu ham'
-  } else {
-    ind = 1
-    // ham.classList.remove('icon-menu');
-    // ham.classList.add('icon-close');
-    ham.className = 'iconfont icon-close ham'
-  }
-}
 
 navItems.forEach((item, ind) => {
   item.addEventListener('click', (e) => {
     e.preventDefault()
     e.stopPropagation()
 
+    header.classList.toggle('active')
+
     navConts[ind].scrollIntoView()
 
     navItems.forEach(item => {
       item.classList.remove('active')
     })
-
-    item.classList.add('active')
-
-    document.body.classList.toggle('lock')
-    nav.classList.toggle('active')
-    ham.classList.toggle('active')
-
-    // hamToggle()
+    item.classList.add('active');
   })
 })
 
 ham.addEventListener('click', (e) => {
-  e.stopPropagation()
-
-  document.body.classList.toggle('lock')
-  nav.classList.toggle('active')
-  ham.classList.toggle('active')
-
-  // hamToggle();
+  header.classList.toggle('active')
 })
