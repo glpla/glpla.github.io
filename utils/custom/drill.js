699fbd39b0be2679477fc76875c15ff23658f264
glpla <1942194789@qq.com>
2024-12-01 19:27:11 +0800

    web

--- utils/custom/drill.js
+++ utils/custom/drill.js
@@ -3,3 +3,16 @@ fetch('/common/drill.html')
     .then(res => {
         document.getElementById('drill').innerHTML = res;
     })
+// function loadModule(url, targetElementId) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById(targetElementId).innerHTML = data;
+//         })
+//         .catch(error => console.error('加载模块时出错:', error));
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     loadModule('header.html', 'header');
+//     loadModule('footer.html', 'footer');
+// });
