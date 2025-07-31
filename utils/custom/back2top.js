7fa65225723eac2abfa5df00c1d787c05f05c172
glpla <1942194789@qq.com>
2025-07-31 15:23:23 +0800

    81

--- utils/custom/back2top.js
+++ utils/custom/back2top.js
@@ -1,4 +1,4 @@
-const back2top = document.querySelector('.back2top')
-document.addEventListener('scroll', () => {
-  back2top.classList.toggle('active', scrollY > innerHeight)
-})
+const back2top = document.querySelector(".back2top");
+document.addEventListener("scroll", () => {
+  back2top.classList.toggle("active", scrollY > innerHeight / 4);
+});
